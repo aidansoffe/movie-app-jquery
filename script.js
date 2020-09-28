d0332021c5a9aaae050376c131b5b42890738d7c
aidansoffe <aidansoffe@gmail.com>
2020-09-27 23:29:53 -0700

    still nothing

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
 
 var movies = []
 
-$(function() {
+$(function () {
 
-$('.submit').click(function() {
+  $('.submit').click(function () {
 
- movies.push($('.movie-name').map(function(){
- return {
-   name: $(this).val(),
-   rating: $(this).next('.movie-rating').val()
- };
-}).get())
-$.each(movies, function(index, val){
-  $.each(val, function(i, va){
-    console.log('name: ', va.name, 'rating: ', va.rating, 'movies:', movies)
-    // $('ul')('<li>' + va.name + " - " + va.rating + "  <span><i class='fa fa-trash'></i></span>")
-  })
-  
-    
-  })
+    movies.push($('.movie-name').map(function () {
+      return {
+        name: $(this).val(),
+        rating: $(this).next('.movie-rating').val()
+
+      };
+
+    }).get())
+
+    $.each(movies, function (index, val) {
+      $.each(val, function (i, va) {
+
+        $('ul').append(`<li>  ${va.name} - ${va.rating}  <span><i class='fa fa-trash'></i></span>`)
+      })
+    })
+    console.log(movies)
+  });
+
+
+
 });
 
 
-
-});
-
-
-
-
-$('ul').on("click", "span", function(){
-$(this).parent().fadeOut(500, function(){
-$(this).remove();
+$('ul').on("click", "span", function () {
+  $(this).parent().fadeOut(400, function () {
+    $(this).remove();
+  })
 })
-})
+
+
 // $(function(){
 //   $('.sorting').on(click(function(){
-//     $('ul').sort()
+//     if(movies.name)
+//     movies.sort(function(a,b){
+//       return a.name > b.name
+//     })
 //   }))
 // })
 
 // // movies sort by alphabetical
-// movies.sort(function(a,b){
-//   return a.name > b.name
-// })
+
 
 // // movies sort by rating
 // movies.sort(function(a,b){
 //   return a.rating > b.rating
 // })
-
-
-
-// console.log(data)
